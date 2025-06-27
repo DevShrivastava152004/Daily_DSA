00fce6640367368e175f6dd538b604cef5df8325
Dev Shrivastava <devshrivastava621@gmail.com>
2025-06-27 22:05:59 +0530

    initial commit

--- Day-16/Stock_Buy_And_Sell.cpp
+++ Day-16/Stock_Buy_And_Sell.cpp
@@ -22,4 +22,4 @@ int main()
     int result = stockBuyandSell(v , n);
     cout<<"Maximum Profit is: "<< result << endl;
     return 0;
-}
+}// this is the optimal solution for the stock buy and sell problem using a single pass algorithm..basically the idea is to keep track of the minimum price seen so far and calculate the profit at each step by subtracting the minimum pricce from the current price. If the profit is greater than the maximum profit seen so far, we update the maximum profit. We also update the minimum price if the current price is lower than the minimum price seen so far.

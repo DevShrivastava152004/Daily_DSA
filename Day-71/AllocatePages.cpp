699f4727d8b39d1eb97ac9d0b2c9127cc7146637
Dev Shrivastava <devshrivastava621@gmail.com>
2025-08-21 08:17:29 +0530

    initial commit

---
+++ Day-71/AllocatePages.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<numeric>
+using namespace std;
+int countStudents(vector<int> & arr,int pages)
+{
+    int n =arr.size();
+    int students = 1;
+    long long pagesStudent = 0;
+    for(int i =0;i<n;i++)
+    {
+        if(pagesStudent + arr[i] <= pages)
+    {
+        pagesStudent += arr[i];
+    } 
+    else {
+        students++;
+        pagesStudent = arr[i];
+    }
+}
+return students;
+}
+int findPages(vector<int>&  arr, int n, int m)
+{
+    if( m > n)
+    return -1;
+    int low = *max_element(arr.begin(), arr.end());
+    int high = accumulate(arr.begin() , arr.end(), 0);
+    for( int pages = low; pages <= high; pages++)
+    {
+        if(countStudents(arr, pages) <= m)
+    {
+        return pages;
+    }
+}
+    return low;
+
+}
+int main()
+{
+    vector<int> arr = {25,46,28,49,24};
+    int n = 5;
+    int m = 4;
+    int ans = findPages ( arr,n,m);
+    cout<< "Minimum number of pages: " << ans;
+    return 0;
+}

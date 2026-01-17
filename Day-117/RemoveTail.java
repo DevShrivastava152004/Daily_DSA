b0889e2659bd30e3b0c4abd979acd81c54fe2af2
Dev Shrivastava <devshrivastava621@gmail.com>
2026-01-17 23:43:53 +0530

    initial commit

--- Day-117/RemoveTail.java
+++
@@ -1,5 +0,0 @@
-public class RemoveTail {
-    public static void main(String[] args) {
-         
-    }
-}
---
+++ Day-119/ReverseDLL.java
@@ -0,0 +1,4 @@
+// we will take a stack data structure which is basically Last In First Out type
+public class ReverseDLL {
+    
+}
